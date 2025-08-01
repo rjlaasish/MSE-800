e19c7df4086c737f12d8b8fcc410e35b8900d8b9
rjlaasish <rjlaasish@gmail.com>
2025-08-02 10:21:34 +1200

    initial commit

--- variables.py
+++ variables.py
@@ -1,21 +1,4 @@
-factorial_number = input("Enter a number to calculate its factorial: ")
+name = 'Aasish Rijal'
+age = 25
 
-
-
-# use while loop
-def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers."
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        result = 1
-        while n > 1:
-            result *= n
-            n -= 1
-        return result
-    
-    
-
-    
-print(factorial(int(factorial_number)))
+print(name,age,sep='\n')
